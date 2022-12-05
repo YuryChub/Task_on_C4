0dd441edddcaf646b65515140c5a2dff8841e0fd
Chub <ychubykin@gmail.com>
2022-12-05 23:10:26 +0300

    Сделал 3 задачи

---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// НЕ ИСПОЛЬЗОВАТЬ Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+double Pow = A;
+
+for (int i = 1; i < B; i++)
+    Pow = Pow*A;
+Console.Write(Pow);
---
+++ Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+string? n = Console.ReadLine();
+int Num = 0, Sum = 0; 
+for (int i = 0; i <= n.Length - 1; i++)
+{
+    Num = Convert.ToInt32(int.Parse(n[i].ToString()));
+    Sum = Sum + Num;
+}
+Console.WriteLine($"Cумма цифр в числе: {Sum}");
